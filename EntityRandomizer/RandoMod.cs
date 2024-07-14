36cc330b4831151e8353dfc698ef3e4c122d7066
synth <wavydave2000@gmail.com>
2024-07-14 01:28:38 -0400

    Add project files.

---
+++ EntityRandomizer/RandoMod.cs
@@ -0,0 +1,32 @@
+﻿using KHMI;
+using KHMI.Types;
+using System.Numerics;
+
+namespace EntityRandomizer
+{
+    public class RandoMod : KHMod
+    {
+        public RandoMod(ModInterface modInterface) : base(modInterface) { }
+
+        public override void playerLoaded(Entity newPlayer)
+        {
+            IntPtr etStart = modInterface.memoryInterface.nameToAddress("FirstEntity");
+            IntPtr etEndPtr = modInterface.memoryInterface.nameToAddress("FinalEntityPtr");
+            IntPtr etEnd = (IntPtr)modInterface.memoryInterface.readLong(etEndPtr);
+            EntityTable et = new EntityTable(modInterface.dataInterface, etStart, etEnd);
+
+            Entity[] entities = et.Entities;
+            Vector3[] positions = new Vector3[entities.Length];
+            for(int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = entities[i].Position;
+            }
+            Random r = new Random();
+            r.Shuffle(positions);
+            for(int i = 0; i < entities.Length; i++)
+            {
+                entities[i].Position = positions[i];
+            }
+        }
+    }
+}
